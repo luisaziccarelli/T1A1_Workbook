b2c1d24576c429f70ec795d8f394f47f4bac6c36
LuisaZ <luisaziccarelli@gmail.com>
2019-12-07 13:57:52 +1100

    1st commit

---
+++ Q14.rb
@@ -0,0 +1,37 @@
+#Q14 Prime numbers
+
+# #def prime (n)
+#     (2..n-1).none? {|divisor| n % divisor == 0 }
+# end
+
+#puts prime(8)
+
+
+#def is_prime(num)
+# n = 2
+#     while n < num
+#       return false if num % n == 0
+#       n += 1
+#     end
+#     true
+#   end
+
+
+#Pseudo-code 
+num = 0...100
+odd = true 
+
+def prime(num)
+n = 
+if num == true && !2
+    puts "Is prime"
+elsif num % num == 0 && num % 1 == 0 
+    puts "Is prime"
+elsif (num % n ) == 0 
+    puts "Is not prime"
+else 
+    puts "it is not prime"
+end 
+end 
+
+puts prime(8)
