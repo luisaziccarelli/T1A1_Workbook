9cac479e654002c43ac76e0bfbbd5deae302db0d
LuisaZ <luisaziccarelli@gmail.com>
2019-12-11 14:20:25 +1100

    3rd commit

---
+++ Q10.rb
@@ -0,0 +1,11 @@
+#Q10 Ruby program to print the type of data
+
+# explicitly assigning vales to variables 
+    a = 3
+    b = 3.0
+    c = "Hello World"
+    
+# printing class of the variables
+    puts "#{a} : #{a.class}"
+    puts "#{b} : #{b.class}"
+    puts "#{c} : #{c.class}"
