499b1adddfe010e6cd96b686d169a4fe69b1a1a3
LuisaZ <luisaziccarelli@gmail.com>
2019-12-11 22:59:29 +1100

    4th commit

--- Q10.rb
+++ Q10.rb
@@ -4,8 +4,17 @@
     a = 3
     b = 3.0
     c = "Hello World"
-    
+    d = :bye 
+    e = 0..10
+    f = { }
+    g = [ ]
+
 # printing class of the variables
     puts "#{a} : #{a.class}"
     puts "#{b} : #{b.class}"
     puts "#{c} : #{c.class}"
+    puts "#{d} : #{d.class}"
+    puts "#{e} : #{e.class}"
+    puts "#{f} : #{f.class}"
+    puts "#{g} : #{g.class}"
+ 
---
+++ Q12.rb
@@ -0,0 +1,16 @@
+#Q12 
+
+# original code 
+# celsius = gets
+# fahrenheit = (celsius * 9 / 5) + 32 
+# print "The result is: " 
+# print fahrenheit 
+# puts "."
+
+# fixed code 
+puts "The temperature in celsius is:"
+celsius = gets.chomp.to_i
+fahrenheit = (celsius * 9 / 5) + 32 
+print "The result in fahrenheit is: #{fahrenheit}" 
+puts "."
+
